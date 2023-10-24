8f60e54bc0dc0840df00d6c5e06a80e9ba8acf61
“designexit” <didmk.lab@gmail.com>
2023-10-24 13:28:56 +0900

    first commit

---
+++ src/main/java/com/mdiary/MDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.mdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MDiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MDiaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mdiary/MDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MDiaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
